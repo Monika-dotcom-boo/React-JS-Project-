e6dc5877d36eb0a4d1c32a878ba85a646fa6ee3b
Monika Rasała <mrasala583@gmail.com>
2024-04-04 10:49:19 +0200

    adding photos to the page,

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import React from 'react'
+import  Navbar from './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src='' alt="" />
+    </nav>
+  )
+}
+
+export default Navbar
