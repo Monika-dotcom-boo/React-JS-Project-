0432103c9147953cfc91b3b79e72f5e2574afcc2
Monika Rasała <mrasala583@gmail.com>
2024-04-04 12:40:31 +0200

    creation using css header

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import React from 'react'
-import  Navbar from './Navbar.css'
+import React from 'react';
+import './Navbar.css'; 
+import logo from "../../assets/logo.png";
 
-const Navbar = () => {
+const CustomNavbar = () => { 
   return (
-    <nav>
-      <img src='' alt="" />
+    <nav className="container"> 
+      <img src={logo} alt=""  className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className='btn'>Contact us</button></li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default CustomNavbar; 
