4782f0c4f795358e27a9b0a2aaa8fb926a89c12a
Monika Rasała <mrasala583@gmail.com>
2024-04-08 13:35:55 +0200

    further work on Testimonials

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Testimonials.css'
+import next_icon from "../../assets/next-icon.png"
+import back_icon from "../../assets/back-icon.png"
+import user_1 from "../../assets/user-1.png"
+import back_icon from "../../assets/back-icon.png"
+
+
+
+function Testimonials() {
+  return (
+    <div className='testimonials'>
+    <img src={next_icon} alt="" class="next-btn"/>
+    </div>
+  )
+}
+
+export default Testimonials
